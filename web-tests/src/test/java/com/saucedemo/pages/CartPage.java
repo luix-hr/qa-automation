46fd890c2acd8c4dba679bfac1d10296eb0766d9
Luiz Henrique <luizh.rocha16@gmail.com>
2026-05-06 17:08:54 -0300

    fix: wait for cart page to fully render before reading items web

--- web-tests/src/test/java/com/saucedemo/pages/CartPage.java
+++ web-tests/src/test/java/com/saucedemo/pages/CartPage.java
@@ -2,18 +2,22 @@ package com.saucedemo.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CartPage extends BasePage {
 
+    private final By cartList = By.cssSelector(".cart_list");
     private final By cartItems = By.cssSelector(".cart_item");
     private final By itemNames = By.cssSelector(".inventory_item_name");
     private final By checkoutButton = By.id("checkout");
 
     public CartPage(WebDriver driver) {
         super(driver);
+        wait.until(ExpectedConditions.urlContains("cart.html"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(cartList));
     }
 
     public int getItemCount() {
