06e7f4be3d6757302ec90d623d579f32fff13fd8
Luiz Henrique <luizh.rocha16@gmail.com>
2026-05-06 13:55:52 -0300

    feat: add cart management test web

---
+++ web-tests/src/test/java/com/saucedemo/tests/CartTest.java
@@ -0,0 +1,34 @@
+package com.saucedemo.tests;
+
+import com.saucedemo.base.BaseTest;
+import com.saucedemo.pages.CartPage;
+import com.saucedemo.pages.InventoryPage;
+import com.saucedemo.pages.LoginPage;
+import com.saucedemo.utils.ConfigManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Cart Management")
+class CartTest extends BaseTest {
+
+    @Test
+    @DisplayName("Adding multiple products updates cart badge")
+    void shouldUpdateCartBadge() {
+        InventoryPage inventory = new LoginPage(driver)
+                .loginAs(ConfigManager.getStandardUser(), ConfigManager.getPassword());
+
+        inventory.addToCart("sauce-labs-backpack");
+        assertEquals(1, inventory.getCartCount());
+
+        inventory.addToCart("sauce-labs-bolt-t-shirt");
+        assertEquals(2, inventory.getCartCount());
+
+        inventory.addToCart("sauce-labs-onesie");
+        assertEquals(3, inventory.getCartCount());
+
+        CartPage cart = inventory.openCart();
+        assertEquals(3, cart.getItemCount());
+    }
+}
